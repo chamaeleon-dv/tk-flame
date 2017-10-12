a5a03323caf06317f64a9aba4f892cb0c973cf8c
Christian Michels <christian.michels@chamaeleon-dv.de>
2017-10-12 15:09:02 +0200

    no message

---
+++ flame_hooks/sg_project_hook.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2014 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+# Note! This file implements the projectHook interface from Flame 2015.2
+        
+def appInitialized(projectName):
+    """
+    Hook called when application is fully initialized.
+    
+    :param projectName: String containing the name of the project that was loaded
+    """
+    
+    # attempt to start the Flame engine if it hasn't already been started.
+    
+    import sgtk
+    import os    
+
+    engine = sgtk.platform.current_engine()
+
+    if engine:
+        # there is already an engine running.
+        # TODO: later on, allow for switching of engines as projects 
+        #       are being switched. For now, issue a warning
+        
+        # Note - Since Flame is a PySide only environment, we import it directly
+        # rather than going through the sgtk wrappers. 
+
+        if engine.get_setting("project_switching") is False:
+            from PySide import QtGui
+            QtGui.QMessageBox.warning(None,
+                                      "No project switching!",
+                                      "The Shotgun integration does not currently support project switching.\n"
+                                      "Even if you switch projects, any Shotgun specific configuration will\n"
+                                      "remain connected to the initially loaded project.")
+    
+    else:
+        # no engine running - so start one!
+        engine_name = os.environ.get("TOOLKIT_ENGINE_NAME")
+        toolkit_context = os.environ.get("TOOLKIT_CONTEXT")
+        
+        if toolkit_context is None:
+            logger = sgtk.LogManager.get_logger(__name__)
+            logger.debug("No toolkit context, can't initialize the engine")
+            return
+        
+        context = sgtk.context.deserialize(toolkit_context)
+        
+        # set a special environment variable to help hint to the engine
+        # that we are running a backburner job
+        os.environ["TOOLKIT_FLAME_ENGINE_MODE"] = "DCC"
+        e = sgtk.platform.start_engine(engine_name, context.sgtk, context)
+        del os.environ["TOOLKIT_FLAME_ENGINE_MODE"]
+        
+        # pass the python executable from the bootstrap to the engine 
+        python_executable = os.environ.get("TOOLKIT_FLAME_PYTHON_BINARY")
+        if not python_executable:
+            e.log_error("Cannot find environment variable TOOLKIT_FLAME_PYTHON_BINARY")
+        else:
+            e.set_python_executable(python_executable)
+
+        install_root = os.environ.get("TOOLKIT_FLAME_INSTALL_ROOT")
+        if not install_root:
+            e.log_error("Cannot find environment variable TOOLKIT_FLAME_INSTALL_ROOT")
+        else:
+            e.set_install_root(install_root)
+
+        # and the version number
+        major_version_str = os.environ.get("TOOLKIT_FLAME_MAJOR_VERSION")
+        minor_version_str = os.environ.get("TOOLKIT_FLAME_MINOR_VERSION")
+        patch_version_str = os.environ.get("TOOLKIT_FLAME_PATCH_VERSION")
+        full_version_str = os.environ.get("TOOLKIT_FLAME_VERSION")
+        
+        if None in (major_version_str, minor_version_str, patch_version_str, full_version_str):
+            e.log_error("Cannot find environment variable TOOLKIT_FLAME_x_VERSION")
+        else:
+            e.set_version_info(major_version_str=major_version_str, minor_version_str=minor_version_str, patch_version_str=patch_version_str, full_version_str=full_version_str)
+        
+        
---
+++ startup.py
@@ -0,0 +1,208 @@
+# Copyright (c) 2016 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+import sys
+
+import sgtk
+from sgtk.platform import SoftwareLauncher, SoftwareVersion, LaunchInformation
+
+
+class FlameLauncher(SoftwareLauncher):
+    """
+    Handles launching Flame executables. Automatically starts up a tk-flame
+    engine with the current context in the new session of Houdini.
+    """
+
+    # A lookup to map an executable name to a product. This is critical for
+    # linux where the product does not show up in the path.
+    EXECUTABLE_TO_PRODUCT = {
+        "flame": "Flame",
+        "flameassist": "Flame Assist",
+        "flare": "Flare",
+        "flamepremium": "Flame Premium",
+    }
+
+    # lookup for icons
+    ICON_LOOKUP = {
+        "Flame": "icon_256.png",
+        "Flame Assist": "flame_assist_icon_256.png",
+        "Flare": "flare_icon_256.png",
+        "Flame Premium": "icon_256.png"
+    }
+
+    # Named regex strings to insert into the executable template paths when
+    # matching against supplied versions and products. Similar to the glob
+    # strings, these allow us to alter the regex matching for any of the
+    # variable components of the path in one place
+    COMPONENT_REGEX_LOOKUP = {
+        "darwin": {
+            "version": "\d.*",  # starts with a number followed by anything
+            "product": "[ \w]+",  # spaces and word characters
+            "app": ".*",  # spaces and word characters
+        },
+        "linux2": {
+            "version": "\d.*",  # starts with a number followed by anything
+            "executable": "[\w]+",  # word characters (a-z0-9)
+        }
+    }
+
+    # This dictionary defines a list of executable template strings for each
+    # of the supported operating systems. The templates are used for both
+    # globbing and regex matches by replacing the named format placeholders
+    # with an appropriate glob or regex string.
+    EXECUTABLE_TEMPLATES = {
+        "darwin": [
+            # /Applications/Autodesk/Flame 2018/Flame 2018.app
+            # /Applications/Autodesk/Flame 2017.1.pr70/Flame 2017.1.pr70.app
+            # /Applications/Autodesk/Flame Assist 2017.1.pr70/Flame Assist 2017.1.pr70.app
+            "/Applications/Autodesk/{product} {version}/{app}.app",
+        ],
+        "linux2": [
+            # /usr/discreet/flame_2017.1/bin/startApplication
+            # /usr/discreet/flameassist_2017.1.pr70/bin/startApplication
+            # /usr/discreet/flare_2017.1/bin/startApplication
+            # /usr/discreet/flamepremium_2017.1/bin/startApplication
+            "/usr/discreet/{executable}_{version}/bin/startApplication",
+            "/opt/Autodesk/{executable}_{version}/bin/startApplication",
+        ]
+    }
+
+    @property
+    def minimum_supported_version(self):
+        """
+        The minimum supported Flame version.
+        """
+        # 2018 was the first version of flame that shipped with taap included.
+        return "2018"
+
+    def prepare_launch(self, exec_path, args, file_to_open=None):
+        """
+        Prepares the given software for launch
+
+        :param str exec_path: Path to DCC executable to launch
+
+        :param str args: Command line arguments as strings
+
+        :param str file_to_open: (optional) Full path name of a file to open on
+            launch
+
+        :returns: :class:`LaunchInformation` instance
+        """
+        # flame comes with toolkit built-in, so no need to
+        # run any startup logic.
+
+        env = {
+            "SHOTGUN_SITE": self.sgtk.shotgun_url,
+            "SHOTGUN_ENTITY_ID": str(self.context.project["id"]),
+            "SHOTGUN_ENTITY_TYPE": str(self.context.project["type"]),
+            "SHOTGUN_ENTITY_NAME": str(self.context.project["name"])
+        }
+
+        return LaunchInformation(exec_path, args, env)
+
+    def scan_software(self):
+        """
+        Scan the filesystem for flame executables.
+
+        :return: A list of :class:`SoftwareVersion` objects.
+        """
+
+        self.logger.debug("Scanning for Flame executables...")
+
+        supported_sw_versions = []
+        for sw_version in self._find_software():
+            (supported, reason) = self._is_supported(sw_version)
+            if supported:
+                supported_sw_versions.append(sw_version)
+            else:
+                self.logger.debug(
+                    "SoftwareVersion %s is not supported: %s" %
+                    (sw_version, reason)
+                )
+
+        return supported_sw_versions
+
+    def _find_software(self):
+
+        # all the executable templates for the current OS
+        executable_templates = self.EXECUTABLE_TEMPLATES.get(sys.platform, [])
+        executable_regexp = self.COMPONENT_REGEX_LOOKUP.get(sys.platform, [])
+
+        # all the discovered executables
+        sw_versions = []
+
+        for executable_template in executable_templates:
+
+            self.logger.debug("Processing template %s.", executable_template)
+
+            executable_matches = self._glob_and_match(
+                executable_template,
+                executable_regexp
+            )
+
+            # Extract all products from that executable.
+            for (executable_path, key_dict) in executable_matches:
+
+                # extract the matched keys form the key_dict (default to None if
+                # not included)
+                executable_version = key_dict.get("version")
+                executable_product = key_dict.get("product")
+                executable_name = key_dict.get("executable")
+                executable_app = key_dict.get("app")
+
+                # we need a product to match against. If that isn't provided,
+                # then an executable name should be available. We can map that
+                # to the proper product.
+                if not executable_product:
+                    executable_product = \
+                        self.EXECUTABLE_TO_PRODUCT.get(executable_name)
+
+                # Unknown product
+                if not executable_product:
+                    continue
+
+                # Adapt the FlameAssist product name
+                if executable_product == "FlameAssist":
+                    executable_product = "Flame Assist"
+
+                # only include the products that are covered in the EXECUTABLE_TO_PRODUCT dict
+                if not executable_product.startswith("Flame") and not executable_product.startswith("Flare"):
+                    self.logger.debug(
+                        "Product '%s' is unrecognized. Skipping." %
+                        (executable_product,)
+                    )
+                    continue
+
+                # exclude Technology demo apps
+                if executable_app and "Technology Demo" in executable_app:
+                    self.logger.debug(
+                        "Ignoring '%s %s - %s'" %
+                        (executable_product, executable_version, executable_app)
+                    )
+                    continue
+
+                # figure out which icon to use
+                icon_path = os.path.join(
+                    self.disk_location,
+                    self.ICON_LOOKUP.get(executable_product, self.ICON_LOOKUP["Flame"])
+                )
+                self.logger.debug("Using icon path: %s" % (icon_path,))
+
+                sw_versions.append(
+                    SoftwareVersion(
+                        executable_version,
+                        executable_product,
+                        executable_path,
+                        icon_path
+                    )
+                )
+
+        return sw_versions
